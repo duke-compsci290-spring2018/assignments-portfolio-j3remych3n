9937542f401e2c7d62b123b453c1c18a98fe1a5b
JEREMY CHEN <jc587@duke.edu>
2018-04-13 22:45:10 -0400

    some progress

--- explore/asdf.js
+++ explore/asdf.js
@@ -1,3 +1,22 @@
-d3.csv("Checking1.csv", function(data) {
-    console.log(data[0]);
-});
+var rawData = {"dates":[], "amt":[], "description":[]};
+
+var loadData = function(){
+d3.csv("/Checking1.csv", function(data) {
+    data.forEach(function(dt) {
+        rawData.dates.push(dt.date);
+        rawData.amt.push(+dt.amt);
+        rawData.description.push(dt.description);
+    });
+});}
+
+var showCircles = function() {
+    d3.select("#circles");
+}
+
+var showHeatmap = function() {
+    d3.select("#date_heatmap");
+}
+
+var showHist = function() {
+    d3.select("#histogram");
+}
