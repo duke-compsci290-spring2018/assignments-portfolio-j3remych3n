88be75db57ada67b6b407c5e2d67e57460c6d118
JEREMY CHEN <jc587@duke.edu>
2018-04-13 23:01:57 -0400

    asdf

--- explore/asdf.js
+++ explore/asdf.js
@@ -12,14 +12,55 @@ d3.csv("/Checking1.csv", function(data) {
 
 loadData();
 
-var showCircles = function() {
-    d3.select("#circles");
+var buble = function() {
+    var width = 750;
+    var height = 750;
+
+    buble.width = function(w){
+        if(!argument.length){
+            return width;
+        }
+        width = w;
+        return buble;
+    }
+
+    buble.height = function(h){
+        if(!argument.length){
+            return height;
+        }
+        height = h;
+        return buble;
+    }
+
+    var michael = function(){
+
+
+
+    }
+
+    return michael;
+    // select svg element
+    var svg = d3.select("#circles"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+    // load bubble d3 pack
+    var bubble = d3.pack()
+        .size([300, 300])
+        .padding(1);
+
+    //create hierarchy
+
 }
 
 var showHeatmap = function() {
-    d3.select("#date_heatmap");
+    var svg = d3.select("#date_heatmap"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 }
 
 var showHist = function() {
-    d3.select("#histogram");
+    var svg = d3.select("#histogram"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 }
